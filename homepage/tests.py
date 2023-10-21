79b920e5508e91560b364f9f3e511a163577834d
ghanahmada <ghanamada@gmail.com>
2023-10-21 09:58:09 +0700

    base authentication

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class HomepageTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/home/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/home/')
+        self.assertTemplateUsed(response, 'homepage.html')
--- litera/settings.py
+++ litera/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'homepage',
+    'authentication',
+    'catalog',
+    'manage_user',
+    'recommendation',
+    'review',
+    'exchange'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'litera.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
