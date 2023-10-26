791c25269c40b140218e9165c8ae3e6ad0dbba92
Andhika Reihan Hervito <andhika.reihan@ui.ac.id>
2023-10-26 09:30:29 +0700

    update recommendation

--- recommendation/urls.py
+++ recommendation/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from recommendation.views import show_recommendation, add_recommendation
+from recommendation.views import show_recommendation, add_recommendation, find_book_id, get_recommendation_json
 
 app_name = 'recommendation'
 
 urlpatterns = [
     path('show-recommendation/', show_recommendation, name='show_recommendation'),
     path('add-recommendation/', add_recommendation, name='add_recommendation'),
+    path('find-book-id/<str:name>/', find_book_id, name='find_book_id'),
+    path('get-recommendation-json/', get_recommendation_json, name='get_recommendation_json')
 ]
--- recommendation/views.py
+++ recommendation/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from .models import Recommendation
 from catalog.models import Book
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import RecommendationForm
+from django.core import serializers
 from django.urls import reverse
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 def show_recommendation(request):
@@ -25,3 +27,12 @@ def add_recommendation(request):
 
     context = {'form': form}
     return render(request, "add_recommendation.html", context)
+
+def find_book_id(request, book_name):
+    book = get_object_or_404(Book, title=book_name)
+
+    return redirect('review:book_review', id=book.id)
+
+def get_recommendation_json(request):
+    rec = Recommendation.objects.all()
+    return HttpResponse(serializers.serialize('json', rec))
