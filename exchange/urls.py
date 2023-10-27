66bf21abe41831a006fbd2a4bcb5c7a53586d73d
raihanakbr <muhammad.raihan22@ui.ac.id>
2023-10-27 20:15:26 +0700

    update: offer logic done

--- exchange/urls.py
+++ exchange/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from exchange.views import show_users, offer_user, get_books
+from exchange.views import show_users, offer_user, get_books, add_offer, show_offers, delete_offer, accept_offer
 
 app_name = 'exchange'
 
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', show_users, name='show_users'),
     path('offer-user/<str:username>/', offer_user, name='offer_user'),
     path('get-books/<str:username>/', get_books, name='get_books'),
+    path('add-offer/', add_offer, name='add_offer'),
+    path('show-offers/', show_offers, name='show_offers'),
+    path('delete-offer/<int:id>/', delete_offer, name='delete_offer'),
+    path('accept-offer/<int:id>/', accept_offer, name='accept_offer'),
 ]
