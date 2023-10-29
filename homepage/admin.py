c9151af82fe1521e0b5a7b3714634bdce00b1cc8
ghanahmada <ghanamada@gmail.com>
2023-10-29 23:07:03 +0700

    FINAL COMMIT BISMILLAH

--- homepage/admin.py
+++ homepage/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from homepage.models import Feedback
 
 # Register your models here.
+@admin.register(Feedback)
+class feedbackAdmin(admin.ModelAdmin):
+    list_display = ['feedback']
